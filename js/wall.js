1b92796ed219ec27d281db85b43a4e6a1a2ee607
Ian Maddox <ianmaddox@google.com>
2019-05-10 15:00:56 -0700

    Misc fixups, removing domain restriction

--- js/wall.js
+++ js/wall.js
@@ -1,4 +1,3 @@
-
 // Table
 var baseHeight = 300;
 var baseWidth = 300;
@@ -6,38 +5,41 @@ var rows = Math.ceil($('body').height() / baseHeight);
 var cols = Math.ceil($('body').width() / baseWidth);
 var cells = rows * cols;
 var t = new table('tableParent', rows,cols);
+var parser = new RSSParser();
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
 var players = {};
 var soundCell = false;
 var feeds = [
-  'PL9HVvEQXdWVaGzUP3ieExFQ6X-KzU2p7z',
   'PL02LhVHOr0eCzWByeX2gHSLA4zCgDaUcd',
   'PL1318A97FDFB405C7',
   'PL1AeKO_GzwBBeYvVCcEt5DurEHBzx-X6E',
   'PL1C494D754EA8475F',
-  'PL2HEDIx6Li8j_jT8JI90WmXeQ657UUKSH',
   'PL2776B5E3711B8338',
+  'PL2HEDIx6Li8j_jT8JI90WmXeQ657UUKSH',
+  'PL2NUgx01KXINGDWZEH7suDv8xyIoum3FP',
   'PL36384B2DAC7D315B',
+  'PL5BtCWdAny03GzqXbLsuDsSbK_1uSMNF2',
   'PL5RwN4-BVR1Lu3JkaYv8JFy-xUY9iFZRh',
   'PL7_Q_mRk-hW8ZNRqnrUEzieoIUJ3MhO6a',
   'PL8AB73568A024A458',
   'PL9996BE0C3C64E92E',
+  'PL9HVvEQXdWVaGzUP3ieExFQ6X-KzU2p7z',
   'PLAEQD0ULngi67rwmhrkNjMZKvyCReqDV4',
   'PLB7DBFEE942B1583A',
-  'PLe8-X14_s3A8pfEUIX7K9UXBqrftchd9H',
   'PLEBA54137F212640D',
   'PLEWHGtSTRB-FCMpNzL5pQ_3GXYMq-Ai8S',
+  'PLEu62FRB9Fxz6ysHnmzBrXugt9UG32lvv',
   'PLEwKmtcft8I_73ekmYLe_nKZA-gkQKfUo',
   'PLF40625A3A2F90B4B',
-  'PLGwahJ8b58WxnF50gsvahn27gkUGPDCqi',
+  'PLG_4mz3l5B2H7iMfPoiNuPbhAglu_lVB-',
   'PLHK0G-RO-wVqzUv55rieH43qLUkNo5SwJ',
   'PLI7BNfpVTvskliZbPyk0P9NzskkaRY5TR',
   'PLIql-6gQAj_razMHNS3gOMzMCvQZ1Arkh',
-  'PLO31xQz4WYWQdiIQFl2kwSfAy2ycYoIN5',
+  'PLJgEVV_uaV5r6tCbi6dzSNHRoQ8sf1MuT',
+  'PLMTp9RfQQN7OhFNnMN_VP60wRenSeBaTn',
   'PLQSe03l3Dk-kIb8Nz1GxZ2ApIiN2srw71',
-  'PLQX2G3HwAfY9gIU0SVrwUr1f7NOY4zdMk',
   'PLQZC74AYy7TO_DGodbWQMkR5XxlAxngTJ',
   'PLR4pl38PnQomU4n8hv8O7gX3s6fTqp1OL',
-  'PLS1tJ5ZUDA-NItYaNHLEPbu09dEjX7KV0',
   'PLSHw43gweakP9jWlvFNl7Zm3UQigEZWbE',
   'PLSWhlpTC-l5Gv9zMl_nAVeC0bCx0EgWfj',
   'PLTOMHeKhfLingODNtYygngllSB89jYIRg',
@@ -45,22 +47,16 @@ var feeds = [
   'PLUs3FEEE2J3nAeayX0MCg-KY00nZayxGn',
   'PLUs3FEEE2J3nY74E81vv9hxWPxUJs885a',
   'PLV8WH6IVZxa8S1AdMelHPQdBeVkoirBnX',
-  'PLV_6flhCqH9WAsGlxswynB3X0_sfBuQLk',
   'PLXhtEIhekVwUSrRWMqHu7wSxRzvOhjUHA',
-  'PLZs0gQed9tMQFDPCtaP_1QmVqOEB4ZGKr',
+  'PLZfhJbHjDHamRH300QWhhjE2Z0P0E_NPo',
+  'PLbDtFvoBybyxcXJ5VrZ2ERUcboT9c9TlC',
+  'PLe8-X14_s3A8pfEUIX7K9UXBqrftchd9H',
   'PLjNhZb2G5ZVigPx3OQn8s1PDyId9fywpO',
   'PLkuWMWdvCcT2ZHtEqQvab4aE34FAIrYUn',
   'PLmNiCGaOQDewerLinorpprY1gmRn9qDrQ',
   'PLomb3VSQH7wg2NixpgeneAWhBp74RHx0b',
   'PLq7JnVtvfZsXizd3OIL02JKkwJ_jmNKbW',
-  'PLuYsjykjEeUMaCitotTlDdLlvsJUf-UAL',
   'PLzYeG2eBkuklNTE_gp9LLs_QnvPnkq2ai',
-  'PLDj0b7PxY4uC5VD-Y42J3tiJAQvQhKkfT',
-  'PLMTp9RfQQN7OhFNnMN_VP60wRenSeBaTn',
-  'PLZfhJbHjDHamRH300QWhhjE2Z0P0E_NPo',
-  'PLEu62FRB9Fxz6ysHnmzBrXugt9UG32lvv',
-  'PLJgEVV_uaV5r6tCbi6dzSNHRoQ8sf1MuT',
-  'PLcbSG-_15Y7V8pAdZhUOmvTik2Op0jCpS'
 ];
 var allVids = [];
 
@@ -92,13 +88,18 @@ setTimeout(() => {
 
 var i = 0;
 console.log("Loading playlists from  youtube...");
-console.log("If you'd like to suggest a playlist, tweet it to https://twitter.com/IanAMaddox")
+
 for(let feedID of feeds) {
-  feed = 'http://www.feedsifter.com/?filters=.&feed=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fplaylist_id%3D' + feedID;
-  RSSParser.parseURL(feed, (err, parsed) => {
-    console.log("Loaded:", parsed.feed.title, "\n"+feedID);
-    parsed.feed.entries.forEach((entry) => {
-      allVids.push(entry.link.replace('http://www.youtube.com/watch?v=', ''));
+  //feed = 'http://www.feedsifter.com/?filters=.&feed=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fplaylist_id%3D' + feedID;
+  feed = CORS_PROXY + 'https://www.youtube.com/feeds/videos.xml?playlist_id=' + feedID;
+  parser.parseURL(feed, (err, parsed) => {
+    if(parsed === undefined) {
+      console.log("Skipping bad playlist " + feed);
+      return;
+    }
+    console.log("Loaded:", parsed.title, "\n"+feedID);
+    parsed.items.forEach((entry) => {
+      allVids.push(entry.link.replace('https://www.youtube.com/watch?v=', ''));
       i++;
     })
   })
@@ -153,7 +154,15 @@ function onPlayerReady(event) {
   }
   // jQuery(event.target.a).show();
   // jQuery(event.target.a).parent().removeClass('toob');
-
+  let id = $(event.target.a).parent().attr("id");
+  setInterval((id) => {
+    // Check in occasionally to ensure the frame isn't hung
+    // console.log("Checking in on vid",id, players[id].getPlayerState());
+    if(players[id].getPlayerState() < 1) {
+      console.log("Unsticking",id);
+      playNext(null, id);
+    }
+  },10000, id);
 }
 
 var done = false;
@@ -180,13 +189,16 @@ function onPlayerStateChange(event) {
     playNext(event);
   }
 }
+
 function vidError(event) {
+  console.log("Vid Error", event);
   let id = $(event.target.a).parent().attr("id");
   ga('send', 'event', 'video', 'error', event.target.getVideoData().video_id, {nonInteraction: true});
   playNext(event);
 }
 
 function play(id) {
+console.log("Playing " + id);
   delete players[id];
   $("#"+id).html('<div id="' + id +'_toob" class="toob"></div><div id="' + id +'_vid" class="vid" style="width:100%;height:100%"></div>');
   $("#" + id + "_toob").removeClass('faded');
@@ -194,7 +206,7 @@ function play(id) {
   let player = new YT.Player( id + '_vid', {
     videoId: video,
     playerVars: {
-      autoplay: 0,
+      autoplay: 1,
       controls: 0,
       suggestedQuality: 'small'
     },
@@ -207,8 +219,8 @@ function play(id) {
   players[id] = player;
 }
 
-function playNext(event) {
-  var id = $(event.target.a).parent().attr("id");
+function playNext(event, id) {
+  id = id === undefined ? $(event.target.a).parent().attr("id") : id;
   play(id);
 }
 
@@ -235,6 +247,9 @@ function unMute(id) {
 }
 
 function fadeOut(id) {
+  if(typeof players[id].getVolume != 'function') {
+    return;
+  }
   let vol = players[id].getVolume();
   if(vol > 33) {
     players[id].setVolume(vol - 33);
